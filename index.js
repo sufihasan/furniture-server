d13806cf55745f53242cf629437eab9286cffed3
sufihasan <hasan6nh@gmail.com>
2024-06-14 22:52:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,292 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASS}@cluster0.owte0be.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+
+        const userCollection = client.db("furnitureDB").collection("users");
+        const furnitureCollection = client.db("furnitureDB").collection("furnitures");
+        const bookingCollection = client.db("furnitureDB").collection("bookings");
+
+        //jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+            res.send({ token });
+
+        });
+
+        //middle ware
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                console.log('error', err);
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+
+            })
+
+        }
+
+
+        //middle ware
+        //use veryfy Seller after verify token
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden' });
+            }
+            next();
+        }
+
+
+        //middle ware
+        //use veryfy admin after verify token
+        const verifySeller = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const isSeller = user?.role === 'seller';
+            if (!isSeller) {
+                return res.status(403).send({ message: 'forbidden' });
+            }
+            next();
+        }
+
+        //----booking related api ----
+        app.post('/booking', async (req, res) => {
+            const bookingInfo = req.body;
+            const result = await bookingCollection.insertOne(bookingInfo);
+            res.send(result);
+        })
+        //----booking related api ----
+
+
+
+        //-----------------------
+        //furniture related api start
+        //-----------------------
+
+        app.post('/furniture', verifyToken, verifySeller, async (req, res) => {
+            const furnitureInfo = req.body;
+            const result = await furnitureCollection.insertOne(furnitureInfo);
+            res.send(result);
+        })
+
+        app.get('/furniture', async (req, res) => {
+            const result = await furnitureCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+        app.delete('/furniture/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+
+            const query = { _id: new ObjectId(id) }
+            const result = await furnitureCollection.deleteOne(query);
+            res.send(result);
+
+
+        })
+
+        app.get('/furniture/advertise', verifyToken, verifySeller, async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            const query = { userEmail: email }
+            const result = await furnitureCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.patch('/furniture/sellerfurniture/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    advertise: 'advertiseOk'
+                }
+            }
+
+            const result = await furnitureCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+
+
+
+        })
+
+
+        //---------------------------
+        //furniture related api end
+        //---------------------------
+
+        //------------------------------------------------------------
+        //------------------------------------------------------------
+
+
+        //-----------------------
+        //user related api start
+        //-----------------------
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
+        //----find only seller from user----start---
+
+        app.get('/users/role/seller', verifyToken, verifyAdmin, async (req, res) => {
+            const role = req.query.role;
+            const query = { role: role };
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/users/role/buyer', verifyToken, verifyAdmin, async (req, res) => {
+            // const role = req.query.role;
+            const query = { role: { $exists: false } };
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+
+        //----find only seller from user----end---
+
+        //make admin
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;  // const id = req.params
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+
+        });
+
+
+
+        //chack admin or not if admin send true else send false ---eddit 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+
+            let admin = false;
+
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+
+            res.send({ admin });
+        })
+
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+
+            let seller = false;
+
+            if (user) {
+                seller = user?.role === 'seller';
+            }
+
+            res.send({ seller });
+        })
+
+
+
+        //---------------------------
+        //user related api start end
+        //---------------------------
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('welcome to furniture treasure');
+})
+
+app.listen(port, () => {
+    console.log(`furnuture treasure is running on port ${port}`)
+})
